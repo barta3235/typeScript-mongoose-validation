d0cf445ba9f294884d397fa68da0267c2e19a900
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-12-13 22:25:06 +0600

    JOI validator

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,17 +1,32 @@
 import { Request, Response } from "express";
 import { StudentServices } from "./student.service";
 import Joi from 'joi'
+import StudentJoiValidationSchema from "./student.validation";
 
 const createStudent= async(req:Request,res:Response)=>{
      try{
-        //send response
-       // we can brin data using param, query and body
-     const student= req.body.student;
-
-
+    
+        // creating a schema using joi
+            //-->  look at student.validate.ts
+     
+    //send response
+    // we can brin data using param, query and body
+    // const student= req.body.student;
+    const {error}= StudentJoiValidationSchema.validate(req.body.student)
 
      // will call the service function to send this data
-     const result= await StudentServices.createStudentIntoDB(student)
+     const result= await StudentServices.createStudentIntoDB(req.body.student)
+
+
+    if(error){
+        res.status(500).json({
+            success:false,
+            message:"Student is created successfuly",
+            data:"Something went wrong",
+            error:error,
+         })
+    }
+
 
      //send response
      res.status(200).json({
@@ -20,12 +35,7 @@ const createStudent= async(req:Request,res:Response)=>{
         data:result,
      })
      }catch(err){
-        res.status(500).json({
-            success:false,
-            message:"Student is created successfuly",
-            data:"Something went wrong",
-            error:err,
-         })
+        console.log(err)
      }
 }
 
---
+++ src/app/modules/student/student.validation.ts
@@ -0,0 +1,22 @@
+import Joi, { defaults } from "joi";
+
+const LocalGuardianJoiValidateSchema= Joi.object({
+    name: Joi.string().required(),
+    occupation:Joi.string().required(),
+    contactNo:Joi.string(),
+    address: Joi.string()
+})
+
+const StudentJoiValidationSchema = Joi.object({
+    id: Joi.string(),
+    name:{
+        firstName: Joi.string().max(20).required(),
+        middleName: Joi.string().max(20),
+        lastName: Joi.string().max(20)
+    },
+    gender: Joi.string().required().valid('male','female','other'), 
+    localGuardian:LocalGuardianJoiValidateSchema.required()
+       
+})
+
+export default StudentJoiValidationSchema
